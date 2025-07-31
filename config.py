dcd950f543386f1ae6bece7a3c7d3e02e81fa3db
music-miko <voilet856@gmail.com>
2025-07-31 08:52:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID = 12345678  # Replace with your API ID
+API_HASH = "your_api_hash"  # Replace with your API HASH
+BOT_TOKEN = "your_bot_token"  # Replace with your bot token
+MONGO_URL = "mongodb://localhost:27017"  # Mongo connection
+ADMIN_ID = 7321657753
